b4760e628828d768df935842489510438e574304
Tyler Moore <tyler.moore53@outlook.com>
2020-04-07 15:04:30 -0700

    Commit re-learning sets

--- PythonApplication console app/PythonApplication_console_app.py
+++ PythonApplication console app/PythonApplication_console_app.py
@@ -8,3 +8,48 @@ print('')
 myTuple = 'Tyler', 26
 print(type(myTuple))
 print(myTuple)
+
+mySet = {1 : "World"}
+mySet.clear()
+mySet = {2 : "Hello"}
+
+#Even Method identifier
+def myfunc(num):
+    if num %2 == 0:
+        return 'Is even'
+    else:
+        return 'Is odd'
+
+def p(a):
+    #shorter print method
+    return print(a)
+
+p("hello")
+
+#Sets will not repeat the same input
+#Sets are not in order
+myset = set(['Hello', 'world', 'hello', 'world', 'myName'])
+
+print('\n')
+print(myset)
+
+
+print('\n')
+print(myfunc(9))
+print('\n')
+
+print('\n')
+print(myfunc(104))
+print('\n')
+
+print('\n')
+print(myfunc(912412241))
+print('\n')
+
+
+print('\n')
+print(myfunc(0))
+print('\n')
+
+print(mySet)
+print(type(mySet))
